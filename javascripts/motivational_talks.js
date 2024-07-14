60e206ccff7051882e2205b08fcc56330a9b9ec3
vicerhatech <victorchideibere@gmail.com>
2024-07-14 19:16:54 +0100

    worked on the social media page

---
+++ javascripts/motivational_talks.js
@@ -0,0 +1,3 @@
+// auto update the copyright footer year
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright').innerHTML = `&copy; ${currentYear} VicerhaTech. All rights reserved.`;
