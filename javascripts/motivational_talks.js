d96c60bc38d2a24f4fcb669d6185570fb80c55f1
vicerhatech <victorchideibere@gmail.com>
2025-01-02 13:55:37 +0100

    deleted old codes files

--- javascripts/motivational_talks.js
+++
@@ -1,3 +0,0 @@
-// auto update the copyright footer year
-const currentYear = new Date().getFullYear();
-document.getElementById('copyright').innerHTML = `&copy; ${currentYear} VicerhaTech. All rights reserved.`;
